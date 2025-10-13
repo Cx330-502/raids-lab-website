e940bc998673e146b0042f16897852a87833cbbe
Cx330-502 <1175963382@qq.com>
2025-10-13 19:44:41 +0800

    update?

--- crater-website/hack/i18n/translation_client.py
+++ crater-website/hack/i18n/translation_client.py
@@ -5,8 +5,8 @@ import time
 from collections import OrderedDict
 import re
 
-API_URL = "http://192.168.5.2:31049/v1/chat/completions"
-MODEL_PATH = "Qwen3-14B"  # 请按需修改
+API_URL = "http://192.168.5.32:32006/v1/chat/completions"
+MODEL_PATH = "Qwen3-30B-A3B-Instruct-2507-Int8"  # 请按需修改
 MAX_RETRIES = 3
 
 # === 辅助函数: 展平 & 重建 JSON ===
